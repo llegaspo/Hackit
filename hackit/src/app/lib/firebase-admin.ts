4acd58b0f884b7b39b2968b17b3c42ec4c251c38
khing2001 <caritero.khingiankeith1@gmail.com>
2025-07-07 23:22:24 +0800

    firebase config

---
+++ hackit/src/app/lib/firebase-admin.ts
@@ -0,0 +1,20 @@
+// src/lib/firebase-admin.ts
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+
+//for validation purposes..
+if (!process.env.FIREBASE_PRIVATE_KEY || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PROJECT_ID) {
+  throw new Error('Missing Firebase admin environment variables');
+}
+
+const firebaseAdminConfig = {
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
+};
+
+const app = !getApps().length ? initializeApp(firebaseAdminConfig) : getApps()[0];
+
+export const auth = getAuth(app);
